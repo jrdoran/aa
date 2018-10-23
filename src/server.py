af973fd40a433def81405e7ed4ad62c1d66883ae
Jim Doran <jrdoran@us.ibm.com>
2018-10-23 13:55:59 -0400

    gggg

--- src/server.py
+++ src/server.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Jim Doran 3"
 
+
+@app.route('/jim')
+def accounts():
+    return "jjjiiimmmaa!"
+
+
+@app.route('/login')
+def login():
+    return "bbbb"
+
 if __name__ == "__main__":
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
 	app.run(host='0.0.0.0', debug=True, port=8080)
